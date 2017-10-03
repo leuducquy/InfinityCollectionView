543b512ce6f69d84215ad1610dcdf27085469d97
quy <leuducquy@codelovers.vn>
2017-10-03 15:24:20 +0700

    Initial Commit

---
+++ InfinityCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InfinityCollectionView
+//
+//  Created by codelovers on 10/3/17.
+//  Copyright © 2017 codelovers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InfinityCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InfinityCollectionView
+//
+//  Created by codelovers on 10/3/17.
+//  Copyright © 2017 codelovers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
